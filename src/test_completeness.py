04d51cfc42d8e874652452aa786d8efc191b0781
mattgh2 <mattwarner744@gmail.com>
2025-11-29 19:21:33 -0600

    tests

--- src/test_completeness.py
+++ src/test_completeness.py
@@ -1,12 +1,37 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 from completeness import struture_completeness
+
+READMES = 500
+SHOW = 3
+
 
 df: pd.DataFrame = pd.read_csv('github_repos_final.csv')
 readmes = list(df['readme'].to_numpy())
 test = struture_completeness(readmes)
-test.compute(3)
+test.compute(READMES)
 
-for i in range(3):
+for i in range(SHOW):
     print(df.iloc[i][['name', 'owner']])
     print(test.get_readme_completeness(readmes[i]))
+
+
+for i in range(READMES):
+    if test.get_readme_completeness(readmes[i])['total'] == 0 and df.iloc[i]['stars'] > 1000:
+        print(df.iloc[i][['name', 'owner']])
+
+x = []
+for i in range(READMES):
+    x.append(test.get_readme_completeness(readmes[i])['total'])
+print(x)
+y = df['stars'][:READMES]
+
+plt.figure(figsize=(15, 10))
+plt.yticks(np.linspace(1, max(y), 15))
+plt.scatter(x, y)
+plt.xlabel('number of sections')
+plt.ylabel('number of stars')
+
+plt.show()
