b393e57acce7fe29721caee999bbf6608747d112
cjb543 <cjb543@protonmail.com>
2025-12-06 23:16:31 -0600

    Add visualizer.py

---
+++ src/visualizer.py
@@ -0,0 +1,6 @@
+# Author: Christopher Benson, Matt Warner
+#
+# Description: Module responsible for visualizing normalized/cleaned data via
+# Matplotlib and Seaborn libraries.
+#
+# Dependencies: Matplotlib, Seaborn, Pandas
